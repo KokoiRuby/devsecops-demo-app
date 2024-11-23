c0fe5a9e3d4b30d913651bfaee1c83f907db9435
Kokoi Ruby <kokoiruby@gmail.com>
2024-11-23 19:08:28 +0800

    v0.1.0

---
+++ foo/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// css
+	r.Static("/static", "./static")
+
+	// load template
+	r.LoadHTMLGlob("templates/*")
+
+	// route
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "DevSecOps Demo App",
+		})
+	})
+
+	// http://localhost:2233
+	err := r.Run(":2233")
+	if err != nil {
+		return
+	}
+}
